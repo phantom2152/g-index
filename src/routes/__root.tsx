9edeb4dc02e032f46eabfd6471af2e8d2f7a8741
Phantom <70034285+phantom2152@users.noreply.github.com>
2026-01-08 11:25:41 +0000

    some smoll changes :)

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,7 +2,6 @@ import { HeadContent, Scripts, createRootRoute } from '@tanstack/react-router'
 import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
 import { TanStackDevtools } from '@tanstack/react-devtools'
 
-
 import appCss from '../styles.css?url'
 
 export const Route = createRootRoute({
@@ -16,13 +15,35 @@ export const Route = createRootRoute({
         content: 'width=device-width, initial-scale=1',
       },
       {
-        title: 'TanStack Start Starter',
+        title: 'G-Drive-Index',
+      },
+      {
+        name: 'description',
+        content: 'Index for Google Drive',
+      },
+      {
+        name: 'theme-color',
+        content: '#000000',
       },
     ],
     links: [
       {
         rel: 'stylesheet',
         href: appCss,
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: '/logo192.png',
+      },
+      {
+        rel: 'apple-touch-icon',
+        href: '/logo192.png',
+      },
+      {
+        rel: 'manifest',
+        href: '/manifest.json',
       },
     ],
   }),
